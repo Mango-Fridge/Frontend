998c649cd24511f7b93dac637f0a9fc4de595218
First Snow <show2633@naver.com>
2025-02-19 11:48:06 +0900

    [Feat]: kakao key 숨김 처리(.env)

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:kakao_flutter_sdk/kakao_flutter_sdk_user.dart';
 import 'package:mango/view/login/exam_apple_login.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 void main() async {
   runApp(const ProviderScope(child: MyApp()));
-  // await dotenv.load(fileName: 'assets/config/.env');
+  await dotenv.load(fileName: 'assets/config/.env');
 
   WidgetsFlutterBinding.ensureInitialized(); // 비동기 작업을 실행하기 전에 위젯 시스템을 초기화
-  KakaoSdk.init(nativeAppKey: '897680a377df9915cefd100eca700720'); // 카카오 초기화
+  KakaoSdk.init(nativeAppKey: dotenv.get('KAKAO_API_KEY')); // 카카오 초기화
 }
 
 class MyApp extends StatelessWidget {
