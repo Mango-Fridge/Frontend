760b07b66585e4e4bfd706e3b85d09a0b7857649
First Snow <show2633@naver.com>
2025-02-19 16:20:47 +0900

    [Build]: Bundle Id 수정 #20

--- lib/viewModel/login/kakao_auth_service.dart
+++ lib/viewModel/login/kakao_auth_service.dart
@@ -12,14 +12,14 @@ class KakaoAuthService implements AbstractAuth {
       try {
         // AccessTokenInfo: 현재 저장된 토큰 확인
         AccessTokenInfo tokenInfo = await UserApi.instance.accessTokenInfo();
-        print('토큰 유효성 체크 성공 ${tokenInfo.id} ${tokenInfo.expiresIn}'); // 아이디와 남은 시간(expiresIn)
+        print(
+          '토큰 유효성 체크 성공 ${tokenInfo.id} ${tokenInfo.expiresIn}',
+        ); // 아이디와 남은 시간(expiresIn)
 
         // 추후 자동로그인 부분에서 토큰 확인한 후, 정보를 가져오기 위함
         // 사용자 정보 가져오기
         User user = await UserApi.instance.me();
-        return UserInfo(
-          email: user.kakaoAccount?.email,
-        );
+        return UserInfo(email: user.kakaoAccount?.email);
       } catch (error) {
         if (error is KakaoException && error.isInvalidTokenError()) {
           print('토큰 만료 $error');
@@ -39,9 +39,7 @@ class KakaoAuthService implements AbstractAuth {
         print('카카오톡으로 로그인 성공');
 
         User user = await UserApi.instance.me();
-        return UserInfo(
-          email: user.kakaoAccount?.email,
-        );
+        return UserInfo(email: user.kakaoAccount?.email);
       } catch (error) {
         print('카카오톡으로 로그인 실패 $error');
 
@@ -57,9 +55,7 @@ class KakaoAuthService implements AbstractAuth {
           print('카카오계정으로 로그인 성공');
 
           User user = await UserApi.instance.me();
-          return UserInfo(
-            email: user.kakaoAccount?.email,
-          );
+          return UserInfo(email: user.kakaoAccount?.email);
         } catch (error) {
           print('카카오계정으로 로그인 실패 $error');
         }
@@ -67,13 +63,12 @@ class KakaoAuthService implements AbstractAuth {
     } else {
       // 카카오톡이 설치X, 카카오계정으로 로그인
       try {
+        print("test");
         await UserApi.instance.loginWithKakaoAccount();
         print('카카오계정으로 로그인 성공');
 
         User user = await UserApi.instance.me();
-        return UserInfo(
-          email: user.kakaoAccount?.email,
-        );
+        return UserInfo(email: user.kakaoAccount?.email);
       } catch (error) {
         print('카카오계정으로 로그인 실패 $error');
       }
@@ -84,7 +79,8 @@ class KakaoAuthService implements AbstractAuth {
   @override
   Future<UserInfo?> logout() async {
     try {
-      await UserApi.instance.logout(); // 로그아웃 하겠다는 메서드, 토큰까지 초기화 -> 다시 로그인하려면 새로운 계정 연결 필요
+      await UserApi.instance
+          .logout(); // 로그아웃 하겠다는 메서드, 토큰까지 초기화 -> 다시 로그인하려면 새로운 계정 연결 필요
       // await UserApi.instance.logout(); // 다시 로그인하면 같은 계정으로 자동 로그인 가능
       print("로그아웃 성공");
       return null;
