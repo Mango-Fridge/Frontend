81881ef1fb5e152702917198b21ac2a1d8421dfd
Bbell <428bbell@gmail.com>
2025-02-22 01:30:42 +0900

    [Feat]: UserInfo - AuthPlatform platform 추가 #26

--- lib/model/login/user_model.dart
+++ lib/model/login/user_model.dart
@@ -1,6 +1,9 @@
 // 사용자 정보를 담기 위한 모델
+import 'package:mango/model/login/platform_auth.dart';
+
 class UserInfo {
+  final AuthPlatform platform; // 플랫폼
   final String? email; // 이메일
 
-  UserInfo({this.email});
+  UserInfo({required this.platform, this.email});
 }
