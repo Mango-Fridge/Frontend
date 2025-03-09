4bae46ff1a7b8708acf54e2d103f2ac584d131f4
jisooohh <a01031141317@gmail.com>
2025-03-09 03:39:05 -0300

    [Refactor]: 코드 약간 정리  #63

--- lib/view/cook/cook_view.dart
+++ lib/view/cook/cook_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:mango/design.dart';
 import 'package:mango/providers/cook_provider.dart';
 import 'generate_cook_view.dart';
 
@@ -10,11 +11,10 @@ class CookView extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final recipeName = ref.watch(recipeNameProvider);
-    final ingredients = ref.watch(ingredientsProvider);
+    final String recipeName = ref.watch(recipeNameProvider);
+    final String ingredients = ref.watch(ingredientsProvider);
 
-    final screenWidth = MediaQuery.of(context).size.width;
-    final screenHeight = MediaQuery.of(context).size.height;
+    final Design design = Design(context);
 
     return Scaffold(
       appBar: AppBar(
@@ -23,7 +23,7 @@ class CookView extends ConsumerWidget {
         centerTitle: true,
       ),
       body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.05),
+        padding: EdgeInsets.symmetric(horizontal: design.screenWidth * 0.05),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
@@ -31,15 +31,17 @@ class CookView extends ConsumerWidget {
 
             // 상단 그룹 선택 박스
             Container(
-              width: screenWidth * 0.3,
-              height: screenHeight * 0.05,
+              width: design.screenWidth * 0.3,
+              height: design.screenHeight * 0.05,
               decoration: BoxDecoration(
                 color: Colors.amber,
                 borderRadius: BorderRadius.circular(8),
               ),
-              padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.03),
-              child: Row(
-                children: const [
+              padding: EdgeInsets.symmetric(
+                horizontal: design.screenWidth * 0.03,
+              ),
+              child: const Row(
+                children: [
                   Icon(Icons.keyboard_arrow_down),
                   SizedBox(width: 5),
                   Text('가족 냉장고', style: TextStyle(fontSize: 14)),
@@ -52,8 +54,8 @@ class CookView extends ConsumerWidget {
             // + 버튼 -> 클릭 시 generate_cook_view로 이동
             Center(
               child: Container(
-                width: screenWidth * 0.9,
-                height: screenHeight * 0.07,
+                width: design.screenWidth * 0.9,
+                height: design.screenHeight * 0.07,
                 decoration: BoxDecoration(
                   color: Colors.amber,
                   borderRadius: BorderRadius.circular(8),
@@ -72,7 +74,7 @@ class CookView extends ConsumerWidget {
             // 요리 정보 표시
             Center(
               child: Column(
-                children: [
+                children: <Widget>[
                   if (recipeName.isNotEmpty && ingredients.isNotEmpty) ...[
                     // 요리 정보가 비워져 있지 않을 때 표시
                     Text(
@@ -84,7 +86,7 @@ class CookView extends ConsumerWidget {
                       '재료: $ingredients',
                       style: const TextStyle(fontSize: 14),
                     ),
-                  ] else ...[
+                  ] else ...<Widget>[
                     // 요리 정보가 비워져 있을 때 표시
                     const Icon(
                       Icons.local_dining,
--- lib/view/cook/generate_cook_view.dart
+++ lib/view/cook/generate_cook_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:mango/design.dart';
 import 'package:mango/providers/cook_provider.dart';
 import 'cook_view.dart';
 
@@ -10,177 +11,168 @@ class GenerateCookView extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     // 텍스트 필드 입력을 관리하기 위한 컨트롤러
-    final _recipeNameController = TextEditingController();
-    final _ingredientsController = TextEditingController();
+    final TextEditingController recipeNameController = TextEditingController();
+    final TextEditingController ingredientsController = TextEditingController();
 
     // 포커스 노드: 텍스트 필드의 포커스 상태 관리 -> 키보드 상태 관리 목적
-    final _recipeNameFocusNode = FocusNode(); // -> 클릭 시 확장도 관리
-    final _ingredientsFocusNode = FocusNode();
-    final _memoFocusNode = FocusNode();
+    final FocusNode recipeNameFocusNode = FocusNode(); // -> 클릭 시 확장도 관리
+    final FocusNode ingredientsFocusNode = FocusNode();
+    final FocusNode memoFocusNode = FocusNode();
 
-    final screenWidth = MediaQuery.of(context).size.width;
-    final screenHeight = MediaQuery.of(context).size.height;
+    final Design design = Design(context);
 
-    return StatefulBuilder(
-      builder: (context, setState) {
-        // 포커스 상태에 따라 resizeToAvoidBottomInset(scaffold에서 bottom sheet 관리하는 속성) 동적 변경
-        final bool shouldResizeForBottomSheet =
-            _memoFocusNode.hasFocus &&
-            !_recipeNameFocusNode.hasFocus &&
-            !_ingredientsFocusNode.hasFocus;
-
-        return Scaffold(
-          // 메모 입력 시에만 키보드 위로 올라옴
-          resizeToAvoidBottomInset: shouldResizeForBottomSheet,
-          appBar: AppBar(
-            title: StatefulBuilder(
-              builder: (context, setState) {
-                // 포커스 상태에 따라 너비 동적으로 변경
-                final isFocused = _recipeNameFocusNode.hasFocus;
-                return Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    // 포커스 상태에 따른 사이즈 변화 시 애니메이션을 위함
-                    AnimatedContainer(
-                      duration: const Duration(milliseconds: 300),
-                      width: isFocused ? screenWidth * 0.75 : null, // 포커스 시 확장
-                      child: IntrinsicWidth(
-                        child: TextField(
-                          controller: _recipeNameController,
-                          focusNode: _recipeNameFocusNode,
-                          decoration: InputDecoration(
-                            hintText: '요리 이름 입력',
-                            suffixIcon: const Icon(Icons.edit, size: 20),
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(8.0),
-                            ),
-                            contentPadding: const EdgeInsets.symmetric(
-                              vertical: 8.0,
-                              horizontal: 12.0,
-                            ),
-                            isDense: true,
-                          ),
-                          style: const TextStyle(fontSize: 16),
-                          onTap: () {
-                            setState(() {}); // 포커스 상태 변경 시 애니메이션 트리거
-                          },
-                          onSubmitted: (_) {
-                            setState(() {}); // 입력 완료 후 상태 업데이트
-                          },
-                        ),
-                      ),
-                    ),
-
-                    // 영양성분 표시 box
-                    if (!isFocused) ...[
-                      const SizedBox(width: 8.0),
-                      Container(
-                        padding: const EdgeInsets.all(8.0),
-                        decoration: BoxDecoration(
-                          color: Colors.grey[200],
+    return Scaffold(
+      appBar: AppBar(
+        title: StatefulBuilder(
+          // ignore: always_specify_types
+          builder: (BuildContext context, setState) {
+            // 포커스 상태에 따라 너비 동적으로 변경
+            final bool isFocused = recipeNameFocusNode.hasFocus;
+            return Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                // 포커스 상태에 따른 사이즈 변화 시 애니메이션을 위함
+                AnimatedContainer(
+                  duration: const Duration(milliseconds: 300),
+                  width:
+                      isFocused ? design.screenWidth * 0.75 : null, // 포커스 시 확장
+                  child: IntrinsicWidth(
+                    child: TextField(
+                      controller: recipeNameController,
+                      focusNode: recipeNameFocusNode,
+                      decoration: InputDecoration(
+                        hintText: '요리 이름 입력',
+                        suffixIcon: const Icon(Icons.edit, size: 20),
+                        border: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(8.0),
                         ),
-                        child: Wrap(
-                          spacing: 15.0,
-                          children: [
-                            Column(
-                              children: const [
-                                Text('열량', style: TextStyle(fontSize: 12)),
-                                Text('300', style: TextStyle(fontSize: 12)),
-                              ],
-                            ),
-
-                            Column(
-                              children: const [
-                                Text('탄', style: TextStyle(fontSize: 12)),
-                                Text('50', style: TextStyle(fontSize: 12)),
-                              ],
-                            ),
-                            Column(
-                              children: const [
-                                Text('단', style: TextStyle(fontSize: 12)),
-                                Text('20', style: TextStyle(fontSize: 12)),
-                              ],
-                            ),
-                            Column(
-                              children: const [
-                                Text('지', style: TextStyle(fontSize: 12)),
-                                Text('10', style: TextStyle(fontSize: 12)),
-                              ],
-                            ),
-                          ],
+                        contentPadding: const EdgeInsets.symmetric(
+                          vertical: 8.0,
+                          horizontal: 12.0,
                         ),
+                        isDense: true,
                       ),
-                    ],
-                  ],
-                );
-              },
-            ),
-          ),
-          body: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              children: [
-                TextField(
-                  decoration: const InputDecoration(
-                    hintText: '냉장고에 있는 음식 재료를 추가해보세요',
-                    border: OutlineInputBorder(),
+                      style: const TextStyle(fontSize: 16),
+                      onTap: () {
+                        setState(() {}); // 포커스 상태 변경 시 애니메이션 트리거
+                      },
+                      onSubmitted: (_) {
+                        setState(() {}); // 입력 완료 후 상태 업데이트
+                      },
+                    ),
                   ),
                 ),
-                SizedBox(height: 200),
-                Text("재료를 추가해주세요."),
-              ],
-            ),
-          ),
 
-          // 바텀 시트
-          bottomSheet: SafeArea(
-            child: Container(
-              height: screenWidth * 0.5,
-              color: Colors.amber,
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  // 메모 입력란
-                  TextField(
-                    decoration: const InputDecoration(
-                      hintText: '요리에 대한 메모를 입력해보세요',
-                      border: OutlineInputBorder(),
-                      filled: true,
-                      fillColor: Colors.white,
+                // 영양성분 표시 box
+                if (!isFocused) ...<Widget>[
+                  const SizedBox(width: 8.0),
+                  Container(
+                    padding: const EdgeInsets.all(8.0),
+                    decoration: BoxDecoration(
+                      color: Colors.grey[200],
+                      borderRadius: BorderRadius.circular(8.0),
                     ),
-                    maxLength: 90,
-                  ),
-                  const SizedBox(height: 10),
+                    child: const Wrap(
+                      spacing: 15.0,
+                      children: <Widget>[
+                        Column(
+                          children: [
+                            Text('열량', style: TextStyle(fontSize: 12)),
+                            Text('300', style: TextStyle(fontSize: 12)),
+                          ],
+                        ),
 
-                  // 추가하기 버튼
-                  ElevatedButton(
-                    onPressed: () {
-                      // 입력값 가져오기
-                      final recipeName = _recipeNameController.text;
-                      final ingredients = _ingredientsController.text;
-
-                      ref.read(recipeNameProvider.notifier).state = recipeName;
-                      ref.read(ingredientsProvider.notifier).state =
-                          ingredients;
-                      Navigator.pop(context); // cook view로 돌아감
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.white,
-                      minimumSize: const Size(double.infinity, 40),
-                    ),
-                    child: const Text(
-                      '추가하기',
-                      style: TextStyle(color: Colors.black),
+                        Column(
+                          children: <Widget>[
+                            Text('탄', style: TextStyle(fontSize: 12)),
+                            Text('50', style: TextStyle(fontSize: 12)),
+                          ],
+                        ),
+                        Column(
+                          children: <Widget>[
+                            Text('단', style: TextStyle(fontSize: 12)),
+                            Text('20', style: TextStyle(fontSize: 12)),
+                          ],
+                        ),
+                        Column(
+                          children: <Widget>[
+                            Text('지', style: TextStyle(fontSize: 12)),
+                            Text('10', style: TextStyle(fontSize: 12)),
+                          ],
+                        ),
+                      ],
                     ),
                   ),
                 ],
+              ],
+            );
+          },
+        ),
+      ),
+
+      // 페이지 내용
+      body: const Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          children: <Widget>[
+            // 검색 필드
+            TextField(
+              decoration: InputDecoration(
+                hintText: '냉장고에 있는 음식 재료를 추가해보세요',
+                border: OutlineInputBorder(),
               ),
             ),
+            SizedBox(height: 200),
+            Text("재료를 추가해주세요."),
+          ],
+        ),
+      ),
+
+      // 바텀 시트
+      bottomSheet: SafeArea(
+        child: Container(
+          height: design.screenWidth * 0.5,
+          color: Colors.amber,
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              // 메모 입력란
+              const TextField(
+                decoration: InputDecoration(
+                  hintText: '요리에 대한 메모를 입력해보세요',
+                  border: OutlineInputBorder(),
+                  filled: true,
+                  fillColor: Colors.white,
+                ),
+                maxLength: 90,
+              ),
+              const SizedBox(height: 10),
+
+              // 추가하기 버튼
+              ElevatedButton(
+                onPressed: () {
+                  // 입력값 가져오기
+                  final String recipeName = recipeNameController.text;
+                  final String ingredients = ingredientsController.text;
+
+                  ref.read(recipeNameProvider.notifier).state = recipeName;
+                  ref.read(ingredientsProvider.notifier).state = ingredients;
+                  Navigator.pop(context); // cook view로 돌아감
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.white,
+                  minimumSize: const Size(double.infinity, 40),
+                ),
+                child: const Text(
+                  '추가하기',
+                  style: TextStyle(color: Colors.black),
+                ),
+              ),
+            ],
           ),
-        );
-      },
+        ),
+      ),
     );
   }
 }
