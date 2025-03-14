b4cc7166e1a274a885c9a0614148ff07f2aab2ed
First Snow <show2633@naver.com>
2025-03-15 01:34:04 +0900

    [Task]: Refrigerator-Refrigerator-마감 임박 반영 #80

--- lib/state/refrigerator_state.dart
+++ lib/state/refrigerator_state.dart
@@ -2,23 +2,36 @@ import 'package:mango/model/content.dart';
 
 class RefrigeratorState {
   List<Content>? contentList;
+  List<Content>? refrigeratorContentList;
+  List<Content>? freezerContentList;
   List<Content>? updateContentList;
+  List<Content>? expContentList;
   bool isUpdatedContent = false;
 
   RefrigeratorState({
     this.contentList,
+    this.refrigeratorContentList,
+    this.freezerContentList,
     this.updateContentList,
+    this.expContentList,
     this.isUpdatedContent = false,
   });
 
   RefrigeratorState copyWith({
     List<Content>? contentList,
+    List<Content>? refrigeratorContentList,
+    List<Content>? freezerContentList,
     List<Content>? updateContentList,
+    List<Content>? expContentList,
     bool? isUpdatedContent,
   }) {
     return RefrigeratorState(
       contentList: contentList ?? this.contentList,
+      refrigeratorContentList:
+          refrigeratorContentList ?? this.refrigeratorContentList,
+      freezerContentList: freezerContentList ?? this.freezerContentList,
       updateContentList: updateContentList ?? this.updateContentList,
+      expContentList: expContentList ?? this.expContentList,
       isUpdatedContent: isUpdatedContent ?? this.isUpdatedContent,
     );
   }
