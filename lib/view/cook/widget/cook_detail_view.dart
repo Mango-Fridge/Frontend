72f60bf1b80ee00c368c94d15cae428f9b69ccf2
jisooohh <a01031141317@gmail.com>
2025-03-26 02:17:23 -0300

    [Feat]: 일치하는 재료 없을 경우 필요 물품 공지 상자 표시하도록 예외처리  #107

--- lib/view/cook/widget/cook_detail_view.dart
+++ lib/view/cook/widget/cook_detail_view.dart
@@ -19,6 +19,12 @@ class _CookDetailViewState extends ConsumerState<CookDetailView> {
       sampleContentList.toList(),
       widget.cook!.cookingItems.toList(),
     );
+
+    final missingIngredients = getMissingCookIngredients(
+      widget.cook!.cookingItems,
+      sampleContentList,
+    );
+
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
@@ -185,42 +191,53 @@ class _CookDetailViewState extends ConsumerState<CookDetailView> {
               }),
 
               // 필요한 물품 알려주는 하단 박스
-              Container(
-                padding: const EdgeInsets.all(16.0),
-                decoration: BoxDecoration(
-                  color: Colors.amber[100],
-                  borderRadius: BorderRadius.circular(8.0),
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: <Widget>[
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
+              // 냉장고에 재료가 부족할 경우 해당 박스 표시
+              missingIngredients.isNotEmpty
+                  ? Container(
+                    padding: const EdgeInsets.all(16.0),
+                    decoration: BoxDecoration(
+                      color: Colors.amber[100],
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: <Widget>[
-                        const Text(
-                          "해당 음식을 만들기 위해 필요한 재료는",
-                          style: TextStyle(fontSize: 14),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: <Widget>[
+                            const Text(
+                              "해당 음식을 만들기 위해 필요한 재료는",
+                              style: TextStyle(fontSize: 14),
+                            ),
+                            Text(
+                              "${getMissingCookIngredients(widget.cook!.cookingItems, sampleContentList).join(', ')} 입니다.",
+                              style: const TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
                         ),
-                        Text(
-                          getMissingCookIngredients(
-                            widget.cook!.cookingItems
-                                .map((item) => item.contentName)
-                                .toList(),
-                            sampleContentList
-                                .map((item) => item.contentName)
-                                .toList(),
-                          ).join(', '),
-                          style: const TextStyle(
-                            fontSize: 14,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        const Text('입니다.', style: TextStyle(fontSize: 14)),
                       ],
                     ),
-                  ],
-                ),
-              ),
+                  )
+                  : Container(
+                    padding: const EdgeInsets.all(16.0),
+                    decoration: BoxDecoration(
+                      color: Colors.green[100], // 배경색 변경 (선택 사항)
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    child: Center(
+                      child: Text(
+                        "현재 냉장고 재료로 ${widget.cook?.cookingName}을(를) 만들 수 있습니다!",
+                        style: const TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ),
             ],
           ),
         ),
@@ -255,15 +272,30 @@ class _CookDetailViewState extends ConsumerState<CookDetailView> {
     );
   }
 
-  Iterable<String> getMissingCookIngredients(
-    List<String> cookIngredientNames,
-    List<String> refrigerIngredientNames,
+  // 냉장고에 존재하지 않는 요리 재료 표시 함수
+  List<String> getMissingCookIngredients(
+    List<Content> cookIngredients,
+    List<Content> refrigerIngredients,
   ) {
-    return cookIngredientNames.where(
-      (name) => !refrigerIngredientNames.contains(name),
-    );
+    // refrigerIngredients에서 카테고리 리스트 추출
+    final refrigerCategories =
+        refrigerIngredients
+            .map((item) => item.category)
+            .where((category) => category != null)
+            .toSet(); // 중복 제거
+
+    // cookIngredients에서 refrigerCategories에 없는 항목 필터링
+    return cookIngredients
+        .where(
+          (item) =>
+              item.category != null &&
+              !refrigerCategories.contains(item.category),
+        )
+        .map((item) => item.contentName)
+        .toList();
   }
 
+  // 일치하는 물품 반환하는 함수
   List<Content> filterContentsByCategory(
     List<Content> RefrigeratorList,
     List<Content> CookingList,
