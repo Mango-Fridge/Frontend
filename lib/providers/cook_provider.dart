26638acff8eb3be0e482bb36fd7af531668a3da2
jisooohh <a01031141317@gmail.com>
2025-03-28 06:31:08 -0300

    [Feat]: Cook list에 Cook 추가 및 Cook 데이터 베이스 저장 함수-provider 변경  #106

--- lib/providers/cook_provider.dart
+++ lib/providers/cook_provider.dart
@@ -1,17 +1,26 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:mango/app_logger.dart';
 import 'package:mango/services/cook_repository.dart';
 import 'package:mango/state/cook_state.dart';
+import 'package:mango/model/cook.dart';
 
 class CookNotifier extends Notifier<CookState?> {
   CookRepository _cookRepository = CookRepository();
+  CookState _cookState = CookState();
+
   @override
   CookState? build() => CookState();
 
-  // cook list load 함수
+  // Cook list load 함수
   Future<void> loadCookList(int groupId) async {
-    state = state?.copyWith(
-      cookList: await _cookRepository.loadCookList(groupId),
-    );
+    try {
+      final List<Cook> cookList = await _cookRepository.loadCookList(groupId);
+      _cookState.cookList = cookList;
+
+      state = state?.copyWith(cookList: _cookState.cookList);
+    } catch (e) {
+      AppLogger.logger.e('[refrigerator_provider/loadContentList]: $e');
+    }
   }
 }
 
